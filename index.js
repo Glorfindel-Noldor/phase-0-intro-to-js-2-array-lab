1df162b7d3e1f8bd7780ac59e5b0da7c7c3bc9d6
Bogar Martinez <bogar.cm@icloud.com>
2023-05-18 19:48:16 -0500

    took longer than it should have

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+};
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+};
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+};
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+};
+function appendCat(name){
+   return appendCat = [...cats,name];
+};
+function prependCat(name){
+    return prependCat = [name, ...cats];
+}
+function removeLastCat(name){
+    return removeLastCat = cats.slice(name,-1);
+}
+function removeFirstCat(name){
+    return removeFirstCat = cats.slice(1,name)
+}
