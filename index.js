2836944880ad277225e902149ebde651608a5cc8
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-03-25 17:29:29 -0500

    quick practice

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const cats = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendCat(name){
-    cats.push(name);
-};
+    return cats.push(name)
+}
+
 function destructivelyPrependCat(name){
-    cats.unshift(name);
-};
-function destructivelyRemoveLastCat(name){
-    cats.pop(name);
-};
-function destructivelyRemoveFirstCat(name){
-    cats.shift(name);
-};
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
 function appendCat(name){
-   return appendCat = [...cats,name];
-};
+    return [...cats, name]
+}
+// 5) appends a cat to the cats array and returns a new array, leaving the cats array unchanged
 function prependCat(name){
-    return prependCat = [name, ...cats];
+    return [name, ...cats]
 }
-function removeLastCat(name){
-    return removeLastCat = cats.slice(name,-1);
+// 6) prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+
+function removeLastCat(){
+    return cats.slice(0,-1)
 }
-function removeFirstCat(name){
-    return removeFirstCat = cats.slice(1,name)
+// 7) removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+
+function removeFirstCat(){
+    return cats.slice(1)
 }
+// 8) removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
